e92ea10c9a66bb347b572399fca2e7c475d31db2
dhia8888 <msitounsi@gmail.com>
2022-06-16 00:38:03 +0100

    add zull gateway

---
+++ apigw/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+m
+=src/main/java/ecommerce/apigateway/ApiGatewayApplication.java,6\1\618d54417abe3685b60cf8eaeb42a969afce7589
---
+++ apigw/src/main/java/com/esprit/apigateway/apigwApplication.java
@@ -0,0 +1,15 @@
+package com.esprit.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+class  apigwApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(apigwApplication.class, args);
+    }
+
+}
+
