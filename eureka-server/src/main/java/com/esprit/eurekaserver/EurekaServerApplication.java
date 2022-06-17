69b8ad61b700403e4fea7f6236f5b76266f9bb28
dhia8888 <msitounsi@gmail.com>
2022-06-17 18:35:56 +0100

    fix issues

--- eureka-server/src/main/java/com/esprit/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/esprit/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package ecommerce.cartservice.eurekaserver;
+package com.esprit.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/com/esprit/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/esprit/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package ecommerce.cartservice.eurekaserver;
+package com.esprit.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
